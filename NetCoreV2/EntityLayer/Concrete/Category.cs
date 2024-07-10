a5cf395c426ac59ddab12e7b838fd40b76bdeff3
muhammetaziz <72401922+muhammetaziz@users.noreply.github.com>
2024-07-11 00:51:27 +0300

    Properties create

--- NetCoreV2/EntityLayer/Concrete/Category.cs
+++ NetCoreV2/EntityLayer/Concrete/Category.cs
@@ -9,5 +9,12 @@ namespace EntityLayer.Concrete
     public class Category
     {
 
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        public string CategoryDescription { get; set; }
+
+        public bool CategoryStatus { get; set; }
+
     }
 }
