a1576bfdc44a77bc363abefc08cbaa81de802810
muhammetaziz <72401922+muhammetaziz@users.noreply.github.com>
2024-07-16 04:01:49 +0300

    sql data input

--- NetCoreV2/NetCoreV2/obj/Debug/net8.0/NetCoreV2.AssemblyInfo.cs
+++ NetCoreV2/NetCoreV2/obj/Debug/net8.0/NetCoreV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+264d5ccc4d7d14cf4ad61c1ec4492a0a4787cab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b3b826d5299824fb15956a96b3e1fb41f65ae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
