db63cef249be97b4b89210cfa10f465220f7ee4e
muhammetaziz <72401922+muhammetaziz@users.noreply.github.com>
2024-07-19 02:20:30 +0300

    19.07.2024
    writer

---
+++ NetCoreV2/NetCoreV2/Controllers/RegisterController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreV2.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager wm=new WriterManager(new EFWriterRepository());
+
+		[HttpGet]
+		public IActionResult Index()
+		{ 
+			return View();
+		}
+		
+
+		[HttpPost]
+		public IActionResult Index(Writer p)
+		{
+			p.WriterStatus=true;
+			p.WriterAbout = "Deneme Test";
+			wm.WriterAdd(p);
+			return RedirectToAction("Index","Blog");
+		}
+	}
+}
+
+
+
+
+//Ekleme işlemi yapılırken HttpGet ile Postun tanımlandıgı ısımlerın aynı olmak zorundadır.
+//HttpGet sayfa yuklenince 
+//HttpPost sayfada butona tıklanınca calısır
--- NetCoreV2/NetCoreV2/obj/Debug/net8.0/NetCoreV2.AssemblyInfo.cs
+++ NetCoreV2/NetCoreV2/obj/Debug/net8.0/NetCoreV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28027af1d590bde136cc859aca266c8e0c727e3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12b32bf6803df3948e3b996eb4ce9ef1d1c6ae61")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
