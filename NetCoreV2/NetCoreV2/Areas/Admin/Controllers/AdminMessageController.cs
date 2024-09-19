07f36f99356490060a3abd60d2ec7732f3de9e64
muhammetaziz <72401922+muhammetaziz@users.noreply.github.com>
2024-09-19 23:59:41 +0300

    1

---
+++ NetCoreV2/NetCoreV2/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreV2.Areas.Admin.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+    }
+}
--- NetCoreV2/NetCoreV2/obj/Debug/net8.0/NetCoreV2.AssemblyInfo.cs
+++ NetCoreV2/NetCoreV2/obj/Debug/net8.0/NetCoreV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34daf6a675d6c00466d7743d39119a6778636352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1960140e824d0bde2ecfccba94838a43bbae218a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCoreV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
